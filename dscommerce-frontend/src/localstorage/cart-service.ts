679362753810e470519e57b418b1c4c18c5e1345
auriceliof <auricelio.suporte@hotmail.com>
2024-02-23 20:12:25 -0300

    07-10 Implementando accessTokenRepository e authService

--- dscommerce-frontend/src/localstorage/cart-service.ts
+++
@@ -1,6 +0,0 @@
-import { OrderDTO } from "../models/order";
-import * as cartRepository from '../localstorage/cart-repository';
-
-export function saveCart(cart: OrderDTO) {
-    cartRepository.save(cart)
-}
