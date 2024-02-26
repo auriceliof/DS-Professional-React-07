ecb574c90c664163512439cdbba21e1bc2aaad22
auriceliof <auricelio.suporte@hotmail.com>
2024-02-26 16:05:49 -0300

    07-15 Redirecionando fora do componente React

--- dscommerce-frontend/src/utils/requests.ts
+++ dscommerce-frontend/src/utils/requests.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { BASE_URL } from "./system";
+import { history } from "./history";
 import * as authService from "../services/auth-service"
 
 export function requestBackend(config: AxiosRequestConfig) {
@@ -34,10 +35,10 @@ axios.interceptors.response.use(
     },
     function (error) {
         if (error.response.status === 401) {
-            console.log("DEU 401!!!");
+            history.push("/login");
         }
         if (error.response.status === 403) {
-            console.log("DEU 403!!!");
+            history.push("/catalog");
         }
     return Promise.reject(error);
         }
