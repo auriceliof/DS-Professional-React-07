faf6a153d87e53f8b866b3fe751e50fac581a59c
auriceliof <auricelio.suporte@hotmail.com>
2024-03-04 20:13:40 -0300

    07-26 Registrando pedido e fazendo ajustes finais

--- dscommerce-frontend/src/services/order-service.ts
+++ dscommerce-frontend/src/services/order-service.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig } from "axios";
 import { requestBackend } from "../utils/requests";
+import { OrderDTO } from "../models/order";
 
 export function findByIdRequest(id: number) {
 
@@ -9,4 +10,16 @@ export function findByIdRequest(id: number) {
     }
 
     return requestBackend(config);
+}
+
+export function placeOrderRequest(cart: OrderDTO) {
+
+    const config : AxiosRequestConfig = {
+        url: "/orders",
+        method: "POST",
+        withCredentials: true,
+        data: cart
+    }
+
+    return requestBackend(config)
 }
